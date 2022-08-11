f2f5a144f0253c03297221d6dc0f28cb8e79f286
hgwxx7 <new_hgw_xx7@protonmail.com>
2022-08-11 15:34:04 +0200

    Add my_select

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -5,7 +5,15 @@ module Enumerable
       block.call(el, i)
       i += 1
     end
-  end   
+  end
+
+  def my_select(&block)
+    return_arr = []
+    self.my_each do |el|
+      return_arr << el if block.call(el)
+    end
+    return_arr
+  end
 end
 
 # You will first have to define my_each
